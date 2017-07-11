134ae54df9c4f13d43fa1dcda323796b6a732283
PradhumanRehal <pradhuman.rehal@gmail.com>
2017-07-11 23:13:06 +0530

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span >%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -86,9 +86,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x=loc.pageX;
+  var y=loc.pageY;
+  logClicks(x,y);
 });
-
 
 
 /*
@@ -104,7 +105,7 @@ Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
-  var locations;
+  var locations
 
   var mapOptions = {
     disableDefaultUI: true
@@ -178,6 +179,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +241,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,188 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//-------------bio---------------------
+var bio ={
+	"name":"Pradhuman Rehal",
+	"role":"Web Developer",
+	"biopic":"images/me.jpg",
+	"welcomeMessage":"Hello there!",
+	"contacts":{
+		"mobile":"9041619702",
+		"twitter":"@Pradhumanrehal",
+		"email":"pradhuman.rehal@coolmail.com",
+		"github":"Pradhumanrehal",
+		"location":"Nabha, Punjab",
+		"blog":"pradhumanrehal.blog"
+	},
+	"skills":["Awesomeness","HTML","css","programming","JS"],
+	"display":function(){
+		var formattedName=HTMLheaderName.replace("%data%",bio.name);
+		var formattedRole=HTMLheaderRole.replace("%data%",bio.role);
+		var formattedWelcomeMessage=HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+		var formattedPic=HTMLbioPic.replace("%data%",bio.biopic);
+
+		
+		$("#header").prepend(formattedName,formattedRole)
+		.append(formattedWelcomeMessage)
+		.append(formattedPic);
+
+	   
+			var formattedMobile=HTMLmobile.replace("%data%",bio.contacts.mobile);
+			var formattedTwitter=HTMLtwitter.replace("%data%",bio.contacts.twitter);
+			var formattedEmail=HTMLemail.replace("%data%",bio.contacts.email);
+			var formattedGithub=HTMLgithub.replace("%data%",bio.contacts.github);
+			var formattedLocation=HTMLlocation.replace("%data%",bio.contacts.location);
+			var formattedBlog=HTMLblog.replace("%data%",bio.contacts.blog);
+
+			$("#topContacts").append(formattedMobile,formattedTwitter,formattedEmail,formattedGithub,formattedLocation,formattedBlog);
+
+		//to check skills
+
+		if(bio.skills.length>0){
+			$("#header").append(HTMLskillsStart);
+			var formattedSkills=HTMLskills.replace("%data%",bio.skills[0]);
+			$("#skills").append(formattedSkills);
+			var formattedSkills=HTMLskills.replace("%data%",bio.skills[1]);
+			$("#skills").append(formattedSkills);
+			var formattedSkills=HTMLskills.replace("%data%",bio.skills[2]);
+			$("#skills").append(formattedSkills);
+			var formattedSkills=HTMLskills.replace("%data%",bio.skills[3]);
+			$("#skills").append(formattedSkills);
+
+		}
+	}
+};
+
+//----------------work------------------------
+
+var work={ 
+	"jobs":[
+		{
+			"employer":"Northrop Grumman corp.",
+			"title":"Aerodynamicist",
+			"location":"Redondo Beach, CA, United States",
+			"dates":"Jan 2017-Future",
+			"description":"The tactic correlates the fascist below each jolly tip."
+		},
+		{
+			"employer":"Boeing",
+			"title":"Structural Analyser",
+			"location":"El Segundo, CA, United States",
+			"dates":"2012-2017",
+			"description":"The tactic correlates the fascist below each jolly tip."
+		}
+	],
+
+	"display":function(){
+		$("#workExperience").append(HTMLworkStart);
+		for(var job in work.jobs){
+		var formattedjobEmployer=HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedjobTitle=HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedjobTimeline=HTMLworkDates.replace("%data%",work.jobs[job].dates);
+		var formattedjobDescription=HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		var formattedjobLocation=HTMLworkLocation.replace("%data%",work.jobs[job].location)
+		$(".work-entry:last").append(formattedjobEmployer,formattedjobTitle,formattedjobTimeline,formattedjobDescription,formattedjobLocation);
+		}
+	}
+};
+
+//-----------------education-------------------
+
+var education={
+	"schools":[
+		{
+			"name":"Govt. Sr. Sec. School",
+			"location":"Nabha ,Punjab",
+			"degree":"Senior Secondary",
+			"major":"Physics",
+			"dates":"2013-2015"
+		},
+
+		{
+			"name":"Bhai Gurdas Inst. of Engg. and Tech.",
+			"location":"Sangrur ,Punjab",
+			"degree":"B.Tech",
+			"major":"Computer Science",
+			"dates":"2015-Present"
+		},
+
+		{
+			"name":"Udacity",
+			"location":"Nabha ,Punjab",
+			"degree":"Nanodegree",
+			"major":"Front end web Developer",
+			"dates":"2017-Present"
+		},
+	],
+
+	"display":function() {
+		$("#education").append(HTMLschoolStart);
+		for(school in education.schools) {		
+			var formattedName = HTMLschoolName.replace("%data%",education.schools[school].name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+			var formattedDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+			var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+			var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[school].major);
+			$(".education-entry:last").append(formattedName + formattedDegree,formattedDates,formattedLocation,formattedMajor);
+		}
+	}
+};
+
+//-----------------projects----------------------
+
+var projects ={
+	"projects":[
+		{
+		"title":"Online Resume",
+		"dates":"2017-Present",
+		"image":"images/project.gif",
+		"description":".......................................",
+		},
+		{
+		"title":"Online Resume",
+		"dates":"2017-Present",
+		"image":"images/project.gif",
+		"description":".......................................",
+		},
+	],
+
+	"display":function(){
+		$("#projects").append(HTMLprojectStart);
+		for(var i in this.projects){
+			var project=this.projects[i];		
+			$(".project-entry:last")
+			.append(HTMLprojectTitle.replace("%data%", project.title) )
+			.append(HTMLprojectDates.replace("%data%", project.dates) )
+			.append(HTMLprojectDescription.replace("%data%", project.description) )
+			.append(HTMLprojectImage.replace("%data%", project.image) );
+			
+		}				
+	}
+};
+
+//---------------internationalize---------------
+
+$("#main").append(internationalizeButton);
+
+var inName=function(){
+	var x = bio.name;
+	x=x.split(" ");
+	x[1]=x[1].toUpperCase();
+	x[0]=x[0].slice(0,1).toUpperCase()+x[0].slice(1).toLowerCase();
+	x=x.join(" ");
+	return x;
+}
+
+//-----------------function-calls---------------- 
+
+bio.display();
+work.display();
+education.display();
+projects.display();
+inName();
+
+//-----------------maps-------------------------
+
+$("#mapDiv").append(googleMap);
+
